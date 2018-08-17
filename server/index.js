f5ae8462227012e7683c0b424710fb9887431e15
Sean Ruhland <sean.ruhland@gmail.com>
2018-08-17 13:08:35 -0700

    nodemon config expree be with routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const next = require("next");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,8 +11,6 @@ app
   .then(() => {
     const server = express();
     const showRoutes = require("./routes/index.js");
-
-    server.use("/api", showRoutes);
 
     server.get("*", (req, res) => {
       return handle(req, res);
